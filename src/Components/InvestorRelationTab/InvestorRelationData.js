34b5f0a612ce063a0904c4aac4568c5e11a1af7e
webtesting22 <150318251+divyam-prepseed@users.noreply.github.com>
2025-09-04 21:50:34 +0530

    Updated new data

--- src/Components/InvestorRelationTab/InvestorRelationData.js
+++ src/Components/InvestorRelationTab/InvestorRelationData.js
@@ -503,12 +503,15 @@ const investorRelationData = [
   },
   {
     name: 'Rights Issue',
-    files: [],
+    files: [
+      { name: 'Final Audit report and Financial Statement 24-25', path: '/Images/Icons/Documents/RightsIssue/Final Audit report and Financial Statement 24-25.pdf' }
+    ],
     children: [
       {
         name: 'Draft letter of offer',
         files: [
-          { name: 'Final DLOF_TCL02092025', path: '/Images/Icons/Documents/RightsIssue/Final DLOF_TCL02092025 (1).pdf' }
+          { name: 'Final DLOF_TCL02092025', path: '/Images/Icons/Documents/RightsIssue/Final DLOF_TCL02092025.pdf' },
+          // { name: 'Final DLOF_TCL02092025 (1)', path: '/Images/Icons/Documents/RightsIssue/Final DLOF_TCL02092025 (1).pdf' }
         ],
         children: []
       }
